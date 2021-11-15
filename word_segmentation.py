79a5f5e57d1c0dc19397f97fa99378c1e1ce9de1
Dang Hoang Minh <43031977+danghoangminh@users.noreply.github.com>
2021-11-15 16:23:57 +0000

    Fix minor bug

--- word_segmentation.py
+++ word_segmentation.py
@@ -2,11 +2,5 @@ from underthesea import word_tokenize
 
 list_word = []
 
-with open("word.txt", "r", encoding="utf-8") as f:
-    text = f.readlines()
-    for line in text:
-        list_word += [word_tokenize(line)]
-
-with open("word_segmentation.txt", "w", encoding="utf-8") as f:
-    for line in list_word:
-        f.write(str(line) + "\n")
+with open('word.txt', 'r', encoding='utf-8') as f:
+    data = f.read()
