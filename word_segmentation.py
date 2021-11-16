6b015649206a3b7275430cab6e57f31d5a3147ed
Dang Hoang Minh <43031977+danghoangminh@users.noreply.github.com>
2021-11-16 04:39:18 +0000

    Update word_segmentation.py

--- word_segmentation.py
+++ word_segmentation.py
@@ -3,4 +3,11 @@ from underthesea import word_tokenize
 list_word = []
 
 with open('word.txt', 'r', encoding='utf-8') as f:
-    data = f.read()
+    lines = f.readlines()
+    for line in lines:
+        list_word.append(word_tokenize(line, format='text'))
+
+with open('word_segmentation.txt', 'w', encoding='utf-8') as f:
+    for word in list_word:
+        for w in word:
+            f.write(w + '\n')
